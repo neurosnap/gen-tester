520cb4b0990507d46c34568d00beee8247a2058c
Eric Bower <neurosnap@gmail.com>
2018-09-03 01:07:37 -0400

    typedefs update

--- index.d.ts
+++ index.d.ts
@@ -11,6 +11,26 @@ declare module 'gen-tester' {
     type: string;
     returns: any;
   }
+  export interface FinishesResult {
+    type: string;
+    expected: any[];
+    returns: any;
+  }
+  export interface StepEvaluater {
+    pass: boolean;
+    expected: any;
+    actual: any;
+    message: (...args: any[]) => string;
+  }
+  interface EvaluateProps {
+    actual: any[];
+    expected: any[];
+    equal: (a: any, b: any) => boolean;
+  }
+  interface StepsProps {
+    actual: any[];
+    expected: any[];
+  }
   export function genTester(
     fn: Function,
     ...args: any[]
@@ -18,4 +38,11 @@ declare module 'gen-tester' {
   export function skip(value?: any): YieldResult;
   export function yields(value: any, returns?: any): YieldResult;
   export function throws(returns: any): ThrowResult;
+  export function finishes(value: any, returns?: any): FinishesResult;
+  export function evaluateSteps({
+    actual,
+    expected,
+    equal,
+  }: EvaluateProps): StepEvaluater;
+  export function stepsToBeEqual(props: StepsProps): StepEvaluater;
 }
